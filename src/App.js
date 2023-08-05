7d0a02f5ce8ead6e558e9789bb0a29ff567067b9
Azikenp <azikenp26@gmail.com>
2023-08-05 16:37:41 +0100

    creating 10 different values fro dice

--- src/App.js
+++ src/App.js
@@ -2,28 +2,26 @@ import React from 'react';
 import Die from './components/Die';
 import './App.css';
 
-
-
-function allNewDice() {
-  const randNum = Math.floor((Math.random() * 6 + 1));
-  console.log(randNum) 
-}
-allNewDice()
  
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+
+  function allNewDice(){
+    const newDice = []
+    for (let i = 0; i < 10; i++){
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+  console.log(allNewDice())
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
     <main>
       <div className="dice--container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
+        {diceElements}
       </div>
     </main>
   )
