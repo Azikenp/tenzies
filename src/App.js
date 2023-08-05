468dad73ca870b24f50b63e006cd03514058fb2c
Azikenp <azikenp26@gmail.com>
2023-08-05 18:26:27 +0100

    roll and hold functionality

--- src/App.js
+++ src/App.js
@@ -5,31 +5,56 @@ import './App.css';
 
  
 export default function App() {
+  /**
+ * Challenge: Create a function `holdDice` that takes
+ * `id` as a parameter. For now, just have the function
+ * console.log(id).
+ * 
+ * Then, figure out how to pass that function down to each
+ * instance of the Die component so when each one is clicked,
+ * it logs its own unique ID property. (Hint: there's more
+ * than one way to make that work, so just choose whichever
+ * you want)
+ * 
+ */
   
-  const [dice, setDice] = React.useState(allNewDice());
+const [dice, setDice] = React.useState(allNewDice());
+
+  function generateNewDie(){
+    return {
+      value: Math.ceil(Math.random() * 6), 
+      isHeld: false,
+      id: nanoid(),
+    }
+  }
 
 
   function allNewDice(){
     const newDice = []
     for (let i = 0; i < 10; i++){
-      newDice.push({
-        value: Math.ceil(Math.random() * 6), 
-        isHeld: false,
-        id: nanoid(),
-      })
+      newDice.push(generateNewDie())
     }
     return newDice
   }
 
+
   function rollDice(){
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld? die: generateNewDie()
+    }))
   }
-  console.log(allNewDice())
+  
+  function holdDice(id){
+    setDice(oldDice => oldDice.map(die => {
+      return die.id ===id? {...die, isHeld: !die.isHeld}: die
+    }))
+  }
 
   const diceElements = dice.map(die => <Die 
     key={die.id} 
     value={die.value} 
     isHeld={die.isHeld}
+    holdDice={() => holdDice(die.id)}
     />)
 
   return (
